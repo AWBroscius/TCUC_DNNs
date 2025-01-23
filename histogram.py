3a8dde23281d1f9b548526ab1716d9765923634a
AWBroscius <abby.broscius@gmail.com>
2025-01-23 15:47:55 -0500

    add model restarting

--- histogram.py
+++ histogram.py
@@ -16,6 +16,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OrdinalEncoder
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
+from modelArchitecture_3LSTM_4FC import TorchModel
 
 """
 Command line inputs:
@@ -27,40 +28,99 @@ Command line inputs:
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device} \n")
 
-
 modelname = sys.argv[2]
 parent_dir = "outputs_from_models"
 location = os.path.join(parent_dir, modelname)
-
 
 # load in data
 datapath = sys.argv[1]
 
 Time_series_X_train = torch.tensor(np.load(os.path.join(datapath, r'T_s_X_train.npy')), dtype=torch.float32).to(device)
-Time_series_Y_train = torch.tensor(np.load(os.path.join(datapath, r'T_s_Y_train_flattened.npy')),
-                                   dtype=torch.float32).to(device)
+Time_series_Y_train = torch.tensor(np.load(os.path.join(datapath, r'T_s_Y_train_flattened.npy')), dtype=torch.float32).to(device)
 Time_series_X_test = torch.tensor(np.load(os.path.join(datapath, r'T_s_X_test.npy')), dtype=torch.float32).to(device)
-Time_series_Y_test = torch.tensor(np.load(os.path.join(datapath, r'T_s_Y_test_flattened.npy')), dtype=torch.float32).to(
-    device)
+Time_series_Y_test = torch.tensor(np.load(os.path.join(datapath, r'T_s_Y_test_flattened.npy')), dtype=torch.float32).to(device)
 idx_y = np.load(os.path.join(datapath, 'y_test_idx.npy'))
 
-print("Nonzero Y test: ", np.count_nonzero(Time_series_Y_test))
 
-print("max Y test: ", np.max(Time_series_Y_test))
-print("min Y test: ", np.min(Time_series_Y_test))
+# boot up model from saved training
+seq_len = Time_series_X_train.shape[1]  # number of timestamps in 1 sample
+num_lines = Time_series_X_train.shape[2]  # number of lines in 1 sample
+learning_rate = 0.001
 
-print("Creating histogram for testing data....")
-plt.hist(Time_series_Y_test)
-plt.savefig('histYtest.png')
 
-print("Nonzero Y train: ", np.count_nonzero(Time_series_Y_train))
+def resume(model, filename):
+    checkpath = os.path.join(location, 'model', filename)
+    model.load_state_dict(torch.load(checkpath, weights_only=True))
 
-print("max Y train: ", np.max(Time_series_Y_train))
-print("min Y train: ", np.min(Time_series_Y_train))
 
-print("Creating histogram for training data....")
-plt.figure(2)
-plt.hist(Time_series_Y_train)
-plt.savefig('histYtrain.png')
+# Instantiate the model
+torchmodel = TorchModel(num_lines=num_lines, seq_len=seq_len, num_layers=1)
+torchmodel.to(device)
+print(torchmodel)
+
+print("\n Using model from: ", sys.argv[3])
+# assume resume file is in format "epoch-##.pth"
+resume(torchmodel, sys.argv[3],)
+
+# Define the loss function and optimizer
+criterion = torch.nn.MSELoss()  # mean squared error
+optimizer = torch.optim.Adam(torchmodel.parameters(),
+                             lr=learning_rate,
+                             betas=(0.9, 0.99),
+                             eps=1e-07)
+
+# use trained model to make predictions
+torchmodel.eval()
+predictions = torchmodel(Time_series_X_test)
+
+
+# add predictions info to histogram
+print("Nonzero predictions: ", np.count_nonzero(predictions.detach().numpy()))
+
+print("max prediction: ", np.max(predictions.detach().numpy()))
+print("min prediction: ", np.min(predictions.detach().numpy()))
+
+predflat = predictions.detach().numpy().flatten()
+
+print("created predflat, size: ", predflat.shape)
+print("Creating histogram....")
+# plt.hist(predflat, label="Predictions", alpha=0.7)
+plt.title(f"Histogram of {modelname} predictions")
+plt.savefig(os.path.join(location, f'hist_{modelname}_Predictions.png'))
+
+# add ground test info
+ytest = np.array(Time_series_Y_test)
+print("Nonzero Y test: ", np.count_nonzero(ytest))
+
+print("max prediction: ", np.max(ytest))
+print("min prediction: ", np.min(ytest))
+
+ytestflat = ytest.flatten()
+
+print("created ytestflat, size: ", ytestflat.shape)
+print("Creating histogram....")
+plt.hist(abs(ytestflat), label="Ytest", alpha=0.7)
+plt.title(f"Histogram of {modelname} predictions vs test values")
+
+plt.legend()
+# plt.savefig(f'hist_{modelname}_pred_vs_ground.png')
+
+# add training set info
+ytrain = np.array(Time_series_Y_train)
+print("Nonzero Y train: ", np.count_nonzero(ytrain))
+
+print("max y train: ", np.max(ytrain))
+print("min y train: ", np.min(ytrain))
+
+ytrainflat = ytrain.flatten()
+
+print("created ytrainflat, size: ", ytrainflat.shape)
+print("Creating histogram....")
+plt.hist(abs(ytrainflat), label="Ytrain", alpha=0.5)
+
+# add legend and save figure
+plt.legend()
+plt.savefig(f'hist_{modelname}_test_train.png')
+
 
 exit()
